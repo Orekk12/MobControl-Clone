3641da02e0c657dd4b34782bdb2210268d33e1f8
keremkz <kurnazkerem@sabanciuniv.edu>
2022-02-12 17:38:27 +0300

    Finished mob shooting from cannon

--- Assets/Scripts/MobMovement.cs
+++ Assets/Scripts/MobMovement.cs
@@ -4,23 +4,31 @@ using UnityEngine;
 
 public class MobMovement : MonoBehaviour
 {
-    [SerializeField] private Vector3 targetPos;
-    [SerializeField] private float moveSpeed;
 
+    [SerializeField] private Vector3 targetPos;
+    [SerializeField] private GameObject targetObj;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private bool onMove = false;
     private Rigidbody m_Rigidbody;
 
-    void Start()
+    void Awake()
     {
-        targetPos = GameObject.Find("GoalFort").transform.position;//debug
         m_Rigidbody = GetComponent<Rigidbody>();
-
-        if (targetPos != Vector3.zero)
+    }
+    public void StartMovement()
+    {
+        if (targetObj != null || targetPos != Vector3.zero && !onMove)
         {
+            targetPos = targetObj.transform.position;
             Vector3 targetDir = targetPos - transform.position;
             m_Rigidbody.AddForce(targetDir * moveSpeed);
+            onMove = true;
         }
     }
-
+    public void SetTargetObj(GameObject tObj)
+    {
+        targetObj = tObj;
+    }
     public void SetTargetPos(Vector3 tPos)
     {
         targetPos = tPos;
--- Assets/Scripts/ShootMob.cs
+++ Assets/Scripts/ShootMob.cs
@@ -5,15 +5,53 @@ using UnityEngine;
 public class ShootMob : MonoBehaviour
 {
     [SerializeField] private ObjectPooler OP;
+    [SerializeField] private Vector3 shootPos;
+    [SerializeField] private Quaternion shootRot;
+    [SerializeField] private float fireCooldown = 1f;
+    [SerializeField] private GameObject enemyFort;
+    private float lastFireTime = 0f;
+    private GameObject shootPivot;
     // Start is called before the first frame update
     void Start()
     {
-        OP = GameObject.Find("Spawner").GetComponent<ObjectPooler>();
+        OP = ObjectPooler.SharedInstance;
+        shootPivot = transform.GetChild(0).gameObject.transform.GetChild(0).gameObject;
+        enemyFort = GameObject.Find("GoalFort");
+    }
+    private void Update()
+    {
+        shootPos = shootPivot.transform.position;
+        shootRot = shootPivot.transform.rotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDrag()
     {
-        
+        if (lastFireTime + fireCooldown <= Time.time)
+        {
+            Shoot(shootPos, shootRot, enemyFort);
+            lastFireTime = Time.time;
+            Debug.Log("shoot");
+        }
     }
+
+    private void Shoot(Vector3 pos, Quaternion rot, Vector3 targetPos)
+    {
+        GameObject playerMob = OP.GetPooledObject(0);
+        playerMob.transform.position = pos;
+        playerMob.transform.rotation = rot;
+        playerMob.SetActive(true);
+        playerMob.GetComponent<MobMovement>().SetTargetPos(targetPos);
+        playerMob.GetComponent<MobMovement>().StartMovement();
+    }
+    private void Shoot(Vector3 pos, Quaternion rot, GameObject targetObj)
+    {
+        GameObject playerMob = OP.GetPooledObject(0);
+        playerMob.SetActive(true);
+        playerMob.transform.position = pos;
+        playerMob.transform.rotation = rot;
+        playerMob.GetComponent<MobMovement>().SetTargetObj(targetObj);
+        playerMob.GetComponent<MobMovement>().StartMovement();
+    }
+
+
 }
